456c790c2cb3ddfc319d8fbe5d9819eb4e2fb7ab
Pablo Elizalde <pelizcar@gmail.com>
2017-12-13 20:49:13 +0100

    Update `tl_classifier.py`

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,74 +1,120 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageColor
+import numpy as np
 
 class TLClassifier(object):
 
-    MIN_THRESHOLD = 0.3
-
-    # Classes 
-    # LABEL_DICT =  {
-    #     "Green" : 1,
-    #     "Red" : 2,
-    #     "GreenLeft" : 3,
-    #     "GreenRight" : 4,
-    #     "RedLeft" : 5,
-    #     "RedRight" : 6,
-    #     "Yellow" : 7,
-    #     "off" : 8,
-    #     "RedStraight" : 9,
-    #     "GreenStraight" : 10,
-    #     "GreenStraightLeft" : 11,
-    #     "GreenStraightRight" : 12,
-    #     "RedStraightLeft" : 13,
-    #     "RedStraightRight" : 14
-    # }
 
     def __init__(self):
 
-        # Set default state
-        self.current_light = TrafficLight.UNKNOWN
+        self.detection_graph = self.load_graph('frozen_inference_graph.pb')
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.detection_number = self.detection_graph.get_tensor_by_name('num_detections:0')
 
-        # Download model and place it in the same folder
-        PATH_TO_MODEL = 'frozen_inference_graph.pb'
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
+    
+    def get_classification(self, image):
+
+        # Get boxes for traffic lights
+        boxes, scores, classes = self.get_boxes_for_traffic_lights(image)
+
+        width, height = image.size
+        box_coords = self.to_image_coords(boxes, height, width)
+
+        for i in range(len(boxes)):
+            ymin = int(box_coords[i][0])
+            xmin = int(box_coords[i][1])
+            ymax = int(box_coords[i][2])
+            xmax = int(box_coords[i][3])
+
+            image_a = np.asarray(image)
+            cropped_image = image_a[ymin:ymax, xmin:xmax, :]
+            image_resized = cv2.resize(cropped_image, (24, 72))
+
+            light_color = self.get_light_classification(image_resized, boxes)
+            if light_color == 0:
+                return TrafficLight.RED
+
+        return TrafficLight.UNKNOWN
+    
+
+    def get_boxes_for_traffic_lights(self, image):
+        with tf.Session(graph=self.detection_graph) as sess:
+
+            img_expanded = np.expand_dims(np.asarray(image, dtype=np.uint8), 0) 
+
+            # Actual detection.
+            (boxes, scores, classes, num) = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes, self.detection_number], 
+                                        feed_dict={self.image_tensor: img_expanded})
+
+            # Remove unnecessary dimensions
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes)
+     
+            # Filter boxes with a confidence score less than `CONFIDENCE_CUTOFF`
+            return self.filter_boxes(0.5, boxes, scores, classes)
+
+
+    def get_light_classification(self, image, boxes):
+
+        with tf.Session(graph=self.detection_graph) as sess:
+                
+            saver = tf.train.import_meta_graph('./traffic_light.ckpt.meta')
+            saver.restore(sess, './traffic_light.ckpt')
+
+            input_image = tf.get_default_graph().get_tensor_by_name("input_image:0")
+            model_output = tf.get_default_graph().get_tensor_by_name("model_output:0")
+
+            classes = sess.run(model_output, {input_image: [image]})[0]
+            return classes.tolist().index(max(classes))
+
+
+    ################# Utils ########################
+
+    def filter_boxes(self, min_score, boxes, scores, classes):
+        """Return boxes with a confidence >= `min_score` and class == 10 (traffic light)""" 
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score and classes[i] == 10:
+                idxs.append(i)
+    
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+        return filtered_boxes, filtered_scores, filtered_classes
+
+
+    def to_image_coords(self, boxes, height, width):
+        """
+        The original box coordinate output is normalized, i.e [0, 1].
+    
+        This converts it back to the original coordinate based on the image size.
+        """
+        box_coords = np.zeros_like(boxes)
+        box_coords[:, 0] = boxes[:, 0] * height
+        box_coords[:, 1] = boxes[:, 1] * width
+        box_coords[:, 2] = boxes[:, 2] * height
+        box_coords[:, 3] = boxes[:, 3] * width
+
+        return box_coords
+
+
+    def load_graph(self, graph_file):
+        """Loads a frozen inference graph"""
+        graph = tf.Graph()
+        with graph.as_default():
             od_graph_def = tf.GraphDef()
-            # Works up to here.
-            with tf.gfile.GFile(PATH_TO_MODEL, 'rb') as fid:
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
-        self.sess = tf.Session(graph=self.detection_graph)
-
-    def get_classification(self, image):
-
-        # Bounding Box Detection.
-        with self.detection_graph.as_default():
-
-        img_expanded = np.expand_dims(img, axis=0)  
-        (boxes, scores, classes, num) = self.sess.run([self.d_boxes, self.d_scores, self.d_classes, self.num_d],
-            feed_dict={self.image_tensor: img_expanded})
-        
-        # Get the max
-        max_score = scores[0]
-        max_class = classes[0]
-
-        self.current_light = TrafficLight.UNKNOWN
-
-        if (max_score > MIN_THRESHOLD):
-            print("Hay algo")
-            if max_class in (1, 3, 4, 10, 11, 12):
-                print("GREEN")
-                self.current_light = TrafficLight.GREEN
-            elif max_class in (2, 5, 6, 9, 13, 14):
-                print("RED")
-                self.current_light = TrafficLight.RED
-            elif max_class in (7):
-                print("YELLOW")
-                self.current_light = TrafficLight.YELLOW
-
-        return self.current_light
+        return graph
